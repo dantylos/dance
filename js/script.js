b970e17126cefaa8ff62a5e6a9163d0c3e29d631
dantylos <danihme89@gmail.com>
2024-05-16 00:28:50 +0200

    adaptive till 280px devices added

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@ $('.owl-carousel').owlCarousel({
   autoplay:true,
   autoplayTimeout:2000,
   responsive:{
-      0:{
+    0:{
+      items:1
+      },
+
+      460:{
           items:2
       },
 
